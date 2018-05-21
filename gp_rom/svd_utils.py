f7d738d66d90a279b51267d9c14af6206e77f3aa
Zoheyr Doctor <zoheyr.doctor@ligo.org>
2018-05-21 12:48:14 -0500

    created svd_utils and waveform_utils

---
+++ gp_rom/svd_utils.py
@@ -0,0 +1,123 @@
+__doc__ = """Functions to perform the SVD on a matrix,
+    project feature vectors in that matrix into the SVD
+    basis, and also deproject back to the original basis.
+    Other SVD-related functions are here as well"""
+__author__ = """Zoheyr Doctor, zoheyr@gmailcom"""
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+def svd_projection(matrix, center=True,get_errors=False, error_level=1e-5,error_type='constant'):
+    """take the singular-value decomposition of a data matrix A 
+    with feature vectors a.
+    return the svd matrices as well as the data projected into the
+    svd basis:
+        M = UsV*
+        c = V*m
+    Covariances in matrix E can also be propagated to variances F in data 
+    in SVD basis:
+        F = V*EV
+
+    Parameters
+    ----------
+    A : array-like (ndata,nfeatures).  Take the svd of this matrix.  
+    get_errors: bool. whether to propagate errors through the svd
+    error_level: float.  Error on input data   
+    error_type: string.  'constant' or 'relative'.  Constant or relative
+        error on input data is propagated to svd basis
+    center: remove the mean of each feature in the data and return 
+        the mean values
+
+    Returns
+    -------
+    c: data matrix in the SVD basis
+    U: left singular matrix
+    s: matrix of singular values
+    V: right singular matrix
+    c_variance: variance on data in SVD basis
+    center_values: mean values of the data matrix before SVD 
+    """
+
+    if center:
+        feature_means = np.mean(A,axis=1)
+        feature_variances = np.std(A,axis=1)
+    U, s, VT = np.linalg.svd(A, full_matrices=True)
+    V = VT.T
+    c = np.array(map(lambda a: np.dot(a, V), A))
+    
+    if get_errors == True:
+        if error_type='relative':
+            # constant relative error
+            c_variance = np.array(map(lambda a: np.diag(np.dot(V.T,np.dot(np.diag(np.power(a,2.)),V))),error_level*A))
+        else:
+        # constant error 
+            c_variance = np.array(map(lambda a: np.diag(np.dot(V.T,np.dot(np.diag(np.power(a,2.)),V))),error_level*np.ones_like(A)))
+        return c,U,s,V,c_variance
+    else:
+        return c,U,s,V
+
+def get_num_comps(s,variance_limit=0.9):
+    """ get number of SVD components needed to capture
+    variance_limit of the variance in the data
+
+    Parameters
+    ----------
+    s: singular value matrix
+    variance_limit: float, amount of variance to be captured
+
+    Returns
+    -------
+    num_comps: number of components to get capture variance_limit
+        of the variance in the data.
+    """
+
+    return np.amin(np.argwhere((np.cumsum(s)/np.sum(s)) > variance_limit))
+
+def plot_svd_eigenvalues(s):
+    """ Plot the svd eigenvalues of svd matrix s
+
+    Parameters
+    ----------
+    s: singular value matrix
+
+    Returns
+    -------
+    f: matplotlib figure
+    ax: matplotlib axes
+    """
+    f,ax = plt.subplots(2,sharex=True)
+    ax[0].plot(np.diag(s))
+    ax[1].plot(np.diag(s)/np.amax(np.diag(s)))
+    ax[0].set_yscale('log')
+    ax[1].set_yscale('log')
+    ax[1].set_xlabel('Eigenvalue Number')
+    ax[0].set_ylabel('Normalized Eigenvalues')
+    ax[1].set_ylabel('Cumulative Eigenvalues')
+
+    return f,ax
+
+def svd_deproject(c,V,errors=None):
+    """
+    Project data matrix c in SVD basis back to 
+    original basis:
+        a = Vc 
+
+    Parameters
+    ----------
+    c: data matrix in SVD basis
+    V: right singular matrix
+    errors: 1-sigma errors on coefficients
+
+    Returns
+    -------
+    a: data matrix in original basis
+    a_variance: marginal variances on data
+    """
+    a = np.array(map(lambda A: np.dot(A, V.T), a)) 
+    if errors not None:
+        a_variance = np.array(map(lambda A: np.diag(np.dot(V.T,np.dot(np.diag(np.power(A,2.)),V))),errors))
+        return a,a_variance
+    else:
+        return a
+
+
---
+++ gp_rom/waveform_utils.py
@@ -0,0 +1,170 @@
+__doc__ = """Functions for generating GW waveforms"""
+__author__ = """Zoheyr Doctor, zoheyr@gmail.com"""
+
+import lal
+import lalsimulation as LS
+from argparse import Namespace
+import scipy.interpolate as ip
+
+def make_FDwaveform(paramdict):
+    """Generate FD waveform from lal.
+    Parameters
+    ----------
+    paramdict: dictionary
+        A dictionary with keys for the different
+        parameters that go into ChooseFDWaveform
+    M : scalar, mass in solar masses
+    q : scalar, mass ratio
+    chi1,chi2: aligned spins
+    approximant_FD: approximant type
+    deltaF: frequency spacing
+    f_min: starting freq
+    f_max: max freq
+
+    Returns
+    -------
+    f: array.  Array of frequency points
+    Hp: h plus array at frequencies in f
+    Hc: h cross array at frequencies in f
+    """
+    
+    # define defaults
+    defaultdict = dict()
+    defaultdict['m1'] = 10.
+    defaultdict['m2'] = 10.
+    for key in ['phiRef','S1x','S1y','S1z','S2x','S2y','S2z','f_ref','z','i','lambda1','lambda2','longAscNodes','eccentricity','meanPerAno']:
+        defaultdict[key]=0.0
+    for key in ['waveFlags','nonGRparams','LALpars']:
+        defaultdict[key]=None
+    defaultdict['r'] = 1e6*lal.PC_SI
+    defaultdict['amplitudeO'] = -1
+    defaultdict['phaseO'] = -1
+    defaultdict['f_min'] = 40.
+    defaultdict['f_max'] = 4098.
+    defaultdict['deltaF'] = 0.5
+    defaultdict['approximant_FD'] = LS.IMRPhenomD
+
+    # update defaults with what is in paramdict
+    for key in paramdict.keys():
+        defaultdict[key] = paramdict[key]
+
+    # turn dictionary keys into variable names:
+    for k,v in defaultdict.items(): exec(k+'=v') 
+
+    m1_SI = m1*lal.MSUN_SI
+    m2_SI = m2*lal.MSUN_SI
+
+    try:
+        # old call signature for lalsimulation waveforms
+        Hp, Hc = LS.SimInspiralChooseFDWaveform(phiRef, deltaF, m1_SI, m2_SI, S1x, S1y, S1z, S2x, S2y, S2z,
+          f_min, f_max, f_ref, r, i, lambda1, lambda2, waveFlags, nonGRparams, amplitudeO, phaseO,
+          approximant_FD)
+        print 'using old lalsimulation waveform signature'
+    except TypeError:
+        # newer call signature for lalsimulation waveforms includes parameters for eccentricity
+        Hp, Hc = LS.SimInspiralChooseFDWaveform(m1_SI, m2_SI,
+                                                S1x, S1y, S1z, S2x, S2y, S2z, 
+                                                r, i, phiRef, 
+                                                longAscNodes, eccentricity, meanPerAno,
+                                                deltaF, f_min, f_max, f_ref, 
+                                                LALpars, 
+                                                approximant_FD)
+
+    f = np.arange(Hp.data.length) * deltaF
+    n = len(f)
+    return f, Hp.data.data, Hc.data.data
+
+def get_geometric_freq(g_low=0.004,g_high=0.15,dg=None):
+    """
+    Create a list of geometric frequencies (M*f) that
+    will be used to sparsely interpolate waveforms
+
+    Parameters
+    ----------
+    g_low: float
+        lower geometric frequency bound
+    g_high: float
+        higher geometric frequency bound
+    dg: float or callable (default None)
+        if float, linear spacing dg, 
+        if callable, callable is iteratively 
+            applied to frequency to get next dg
+        if none,
+            the phase interpolation prescription from Purrer 2014
+
+    Returns
+    -------
+    g: array of geometric frequencies
+    """
+        
+    if dg == None:
+        # gives a good balance of resolution at low and high frequencies: 
+        # a bit worse than greedy points for low masses near q=1; much better at the high q end.
+        dgfunc = lambda f: 0.3*(f**(4.0/3.0))     
+    elif isinstance(dg,float):
+        dgfunc = lambda f: dg
+    else:
+        dgfunc = dg
+
+    g = [g_low];
+    while (g[-1]<g_high):
+        pt = g[-1] + dgfunc(g[-1]);
+        if (pt<=g_high):
+            g.append(pt);
+        else:
+            g.append(g_high);
+    return np.array(g)
+
+def interpolate_amp_phase(gA,gPhi,Mf,h):
+    """
+    Generate waveform and interpolate onto sparse 
+    geometric frequency grid
+
+    Parameters
+    ----------
+    gA: array
+        geometric frequency array for amplitudes
+    gPhi: array
+        geometric frequency array for phases
+    Mf: array
+        geometric freq array for h
+    h: array
+        strain array
+
+    Returns
+    -------
+    amps: list
+        list of amplitudes at geometric freqs gA
+    phis: list
+        list of phases at geometric freqs gPhi
+    """
+    amp = np.abs(h)
+    phi = np.unwrap(np.angle(h))
+    mask = amp > 0.
+    ampI = ip.InterpolatedUnivariateSpline(Mf[mask], amp[mask])
+    phiI = ip.InterpolatedUnivariateSpline(Mf[mask], phi[mask])
+    return [ampI(gA), phiI(gPhi)]
+
+def get_sparse_waveform(paramdict,**kwargs):
+    """
+    Given a dictionary of binary parameters, 
+    compute the sparse waveform.  
+
+    Parameters
+    ---------
+    paramdict: dictionary of binary parameters
+
+    Returns
+    --------
+    amp: list of amplitudes
+    phi: list of phases
+    """
+    f,hp,hc = make_FDwaveform(paramdict)
+    gA = get_geometric_freq 
+
+    
+
+
+
+
+
